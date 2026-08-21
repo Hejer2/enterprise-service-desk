56af405f4a33014f46612900dec2047106e18550
hajer <hajerjb2@gmail.com>
2026-08-21 14:30:19 +0200

    fix: resolve mobile Flutter CI failure

--- mobile/test/widget_test.dart
+++ mobile/test/widget_test.dart
@@ -4,21 +4,19 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:mobile/main.dart';
+import 'package:mobile/models/user.dart';
 import 'package:mobile/screens/auth/login_screen.dart';
 import 'package:mobile/repositories/auth_repository.dart';
 
 class MockAuthRepository implements AuthRepository {
   @override
-  dynamic noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
-
-  @override
-  Future<dynamic> login(String email, String password) async => null;
+  Future<User?> login(String email, String password) async => null;
 
   @override
   Future<void> logout() async {}
 
   @override
-  Future<dynamic> getCurrentUser() async => null;
+  Future<User?> getCurrentUser() async => null;
 }
 
 void main() {
